ec1a6d60bd336ba239c895d6fa128c72c5ad0a0d
Walter Fritsche <wfritsche95@protonmail.com>
2024-10-02 10:39:04 -0500

    added GET /files

--- controllers/filesController.ts
+++ controllers/filesController.ts
@@ -16,10 +16,124 @@ import {
   injectFormatBytesIntoLocals,
   injectHEIntoLocals,
 } from "../middleware/utils";
+import { Prisma } from "@prisma/client";
 
-export const filesGet = (req: Request, res: Response, next: NextFunction) => {
-  res.render("index", { title: "Your Files" });
-};
+export const filesGet = [
+  isLoggedIn,
+  injectHEIntoLocals,
+  injectDateTimeIntoLocals,
+  query("limit")
+    .optional()
+    .isInt({ min: 5, max: 20 })
+    .withMessage("Limit should be a whole number and between 5 and 20"),
+  query("q")
+    .optional()
+    .trim()
+    .isLength({ max: 50 })
+    .withMessage("search should be shorter than 50 characters"),
+  query("page").optional().isInt(),
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const valResult = validationResult(req);
+    const data = matchedData(req);
+
+    if (!valResult.isEmpty()) {
+      const validErrors = valResult.mapped();
+      const totalFiles = await db.file.count({
+        where: { createdBy: req.user?.id },
+      });
+      const totalPages = Math.ceil(totalFiles < 1 ? 1 : totalFiles / 10);
+      const pagesArr: number[] = Array.from({ length: totalPages }).map(
+        (_, idx) => idx + 1,
+      );
+      const files = await db.file.findMany({
+        where: {
+          createdBy: req.user?.id,
+        },
+        orderBy: {
+          updatedAt: "desc",
+        },
+        take: 10,
+        include: {
+          inFolder: true,
+        },
+      });
+      const query = validErrors.q ? "" : data.q;
+
+      res.render("files", {
+        title: `${req.user?.firstName}'s Files`,
+        files: files,
+        validErrors,
+        currentPage: 1,
+        limit: 10,
+        query: query,
+        pagesArr: pagesArr,
+      });
+      return;
+    }
+
+    let page = Number(data.page || "1");
+    const limit = Number(data.limit || "10");
+
+    const search: Prisma.FileWhereInput = data.q
+      ? {
+          AND: [
+            {
+              createdBy: req.user?.id,
+            },
+            {
+              name: {
+                contains: data.q,
+                mode: "insensitive",
+              },
+            },
+          ],
+        }
+      : {
+          createdBy: req.user?.id,
+        };
+    const totalFiles = await db.file.count({
+      where: search,
+    });
+    const totalPages = Math.ceil(totalFiles / limit);
+    if (page > totalPages) page = totalPages < 1 ? 1 : totalPages;
+    const offset = (page - 1) * limit;
+    const pagesArr: { href: string; num: number }[] = Array.from({
+      length: totalPages,
+    }).map((_, idx) => {
+      const url = new URL("http://localhost:3000/files");
+      if (data.q) {
+        url.searchParams.set("q", data.q);
+      }
+      url.searchParams.set("limit", limit.toString());
+      url.searchParams.set("page", (idx + 1).toString());
+      return {
+        href: url.href,
+        num: idx + 1,
+      };
+    });
+
+    const files = await db.file.findMany({
+      where: search,
+      orderBy: {
+        updatedAt: "desc",
+      },
+      take: limit,
+      skip: offset,
+      include: {
+        inFolder: true,
+      },
+    });
+
+    res.render("files", {
+      title: "Your files",
+      files,
+      currentPage: page,
+      limit: limit,
+      query: data.q,
+      pagesArr: pagesArr,
+    });
+  }),
+];
 
 export const fileGet = [
   isLoggedIn,
@@ -44,7 +158,7 @@ export const fileGet = [
       },
     });
 
-    console.log("filesController.ts -> fileGet() -> file{}", file);
+    // console.log("filesController.ts -> fileGet() -> file{}", file);
 
     if (!file) {
       next(new HttpError("File does not exist", 404));
